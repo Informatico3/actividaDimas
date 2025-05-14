e28a45ae8350c585215c611d630675bdf02585b4
Dimas joel <130826236+Informatico3@users.noreply.github.com>
2025-05-13 22:10:07 -0600

    Update app.js

--- backend-estudiantes/app.js
+++ backend-estudiantes/app.js
@@ -1,29 +1,37 @@
-require('dotenv').config();
-const express = require('express');
+require('dotenv').config(); 
 const mongoose = require('mongoose');
 const session = require('express-session');
 const helmet = require('helmet');
 const cors = require('cors');
 const app = express();
-const errorMiddleware = require('./middlewares/errorMiddleware');
+const errorMiddleware = require('./middlewares/errorMiddleware'); 
 
 app.use(express.json());
+
+
 app.use(helmet());
-app.use(cors());
+
+
+
+
 app.use(session({
-  secret: 'clave_secreta',
+  secret: 'joellemus', 
   resave: false,
   saveUninitialized: true
 }));
+
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB conectado'))
   .catch(err => console.error('Error al conectar MongoDB:', err));
 
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/estudiantes', require('./routes/estudiantesRoutes'));
+// Rutas
+app.use('/api/auth', require('./routes/authRoutes')); 
+app.use('/api/estudiantes', require('./routes/estudiantesRoutes')); 
+
 
 app.use(errorMiddleware);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
